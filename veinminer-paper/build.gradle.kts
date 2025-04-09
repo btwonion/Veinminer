3cddd486f8cdd67c103ae6a956bd07f22b1b74e9
btwonion <btwonion@gmx.de>
2025-04-09 23:59:26 +0200

    add logic to release paper code to nyon repo

--- veinminer-paper/build.gradle.kts
+++ veinminer-paper/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     `shadow-script`
     `publish-script`
     id("io.github.dexman545.outlet")
+    `maven-publish`
 }
 
 dependencies {
@@ -36,5 +37,35 @@ modrinth {
         // The scope can be `required`, `optional`, `incompatible`, or `embedded`
         // The type can either be `project` or `version`
 //        required.project("fabric-api")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "nyon"
+            url = uri("https://repo.nyon.dev/releases")
+            credentials {
+                username = providers.environmentVariable("NYON_USERNAME").orNull
+                password = providers.environmentVariable("NYON_PASSWORD").orNull
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "de.miraculixx"
+            artifactId = "veinminer"
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+
+    JavaVersion.VERSION_21.let {
+        sourceCompatibility = it
+        targetCompatibility = it
     }
 }
